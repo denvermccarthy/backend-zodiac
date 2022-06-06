02d3ee9d376057dcbdf9a8bad11c149da7e8f863
Denver McCarthy <dmichaelspdx@gmail.com>
2022-06-06 12:36:06 -0700

    first red test for /zodiac

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,18 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const zodiac = require('../data/zodiac');
 
 describe('zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  test('/zodiac should return a list of zodiac data', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiac;
+
+    expect(res.body).toEqual(expected);
   });
 
   afterAll(() => {
